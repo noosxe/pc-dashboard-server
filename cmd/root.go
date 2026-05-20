41cfdce4b7893eeaf1e5998d1c3233d454eaf9e8
Levon Kirakosyan <noosx.e@gmail.com>
2026-05-20 16:54:57 +0400

    Bootstrap Go CLI application using Cobra

---
+++ cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "pc-dashboard-server",
+	Short: "A server for managing and monitoring PC dashboards",
+	Long: `PC Dashboard Server is a backend application designed to collect,
+manage, and monitor system metrics, performance logs, and display dashboards.`,
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the RootCmd.
+func Execute() {
+	err := RootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
